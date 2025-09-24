93fc98d8321cdb42c9aa841b2192c8d0369025b5
yadavrajeev07 <rajeevyadavraju7@gmail.comgit congif --global user.name Rajeev>
2025-09-24 17:00:17 +0530

    Update build.gradle and gradle.propertiess

--- android/app/src/main/java/com/yadavrajeev07/helperjiattendance/MainApplication.kt
+++ android/app/src/main/java/com/yadavrajeev07/helperjiattendance/MainApplication.kt
@@ -1,59 +1,37 @@
 package com.yadavrajeev07.helperjiattendance
 
 import android.app.Application
-import android.content.res.Configuration
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
-import com.facebook.react.common.ReleaseLevel
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.mapbox.rctmgl.MapboxPackage
-import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-        this,
-        object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    // Add Mapbox package manually
-                    add(MapboxPackage())
-                }
-
-            override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
         }
-    )
 
-    override val reactHost: ReactHost
-        get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+        override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages.toMutableList()
+            // Mapbox removed
+            return packages
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
 
     override fun onCreate() {
         super.onCreate()
-        DefaultNewArchitectureEntryPoint.releaseLevel = try {
-            ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
-        } catch (e: IllegalArgumentException) {
-            ReleaseLevel.STABLE
-        }
-
-        // Set your Mapbox access token here
-        com.mapbox.maps.Mapbox.getInstance(applicationContext,
-            "sk.eyJ1IjoicmFqZWV2MDciLCJhIjoiY21md3ZiZGdhMDRsbjJrc2I4dzJidGU4byJ9.ZLXeNdcK9cJfoCbC2SqoQw"
-        )
-
-        loadReactNative(this)
-        ApplicationLifecycleDispatcher.onApplicationCreate(this)
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+        SoLoader.init(this, /* native exopackage */ false)
+        // Mapbox initialization removed
     }
 }
